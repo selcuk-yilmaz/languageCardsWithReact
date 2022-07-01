890e5612686f3e38ddd869cb3941ff99fbbfc97e
selcuklu <yilmax4@gmail.com>
2022-07-01 11:41:31 +0300

    language Card Complated

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import reactLogo from "../../assets/react.svg"
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="logo-container">
+        <img className="react-logo" src={reactLogo} alt="" />
+    </div>
+  )
+}
+
+export default Header
